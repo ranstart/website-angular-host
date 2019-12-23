e0d148cd1556f911928b5f2c0ed368821b3ea810
csch961207 <1107554233@qq.com>
2019-12-23 18:33:44 +0800

    日常提交

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    loadChildren: () => import('./lazy-libs/home-wrapper.module').then(m => m.HomeWrapperModule),
     data: {
       routes: {
         name: '::Menu:Home',
---
+++ src/app/lazy-libs/home-wrapper.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { HomeModule } from '@ran-ng/home';
+
+@NgModule({
+  imports: [HomeModule],
+})
+export class HomeWrapperModule {}
